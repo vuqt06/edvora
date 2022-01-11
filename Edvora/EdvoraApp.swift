ae3f7a9440b5dfe14e04428731f3a070f9ad3525
vuqt06 <vuqt@umich.edu>
2022-01-10 21:40:19 -0500

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Vu Trinh on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
